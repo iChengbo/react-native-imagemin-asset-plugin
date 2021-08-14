78488a7b496559d797f77ac27b51f169fafae031
iChengbo <chengbo_x@163.com>
2021-08-14 10:11:35 +0800

    🎉 init demo

---
+++ example/App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+} from 'react-native';
+
+import {Header} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: '#fff',
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <Image source={require('./assets/pic.png')} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;

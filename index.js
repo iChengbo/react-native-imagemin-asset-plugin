3afcc3216e780d9908f6d1ef3c162dfde2c49bfa
iChengbo <chengbo_x@163.com>
2021-08-23 17:29:54 +0800

    feat: support more imagemin plugins

--- index.js
+++ index.js
@@ -1,36 +1,85 @@
 const Metro = require('metro');
-const imagemin = require('imagemin');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminMozjpeg = require('imagemin-mozjpeg');
 const path = require('path');
 
+const imagemin = require('imagemin');
+// lossy plugins
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminGifLossy = require('imagemin-giflossy');
+const imageminSvgO = require('imagemin-svgo');
+const imageminWebP = require('imagemin-webp');
+// lossless plugins
+const imageminJpegTran = require('imagemin-jpegtran');
+const imageminOptiPng = require('imagemin-optipng');
+const imageminGifSicle = require('imagemin-gifsicle');
+
 const defaultConfig = {
+    test: /\.(png|jpg|jpeg)$/, // TODO:RegExp, minimatch glob ...
     imageminDir: './node_modules/react-native-imagemin-asset-plugin/.compressed-images',
-    pngquant: {},
     mozjpeg: {},
+    pngquant: {},
+    giflossy: null,
+    svgo: null,
+    webp: null,
+    jpegtran: null,
+    optipng: null,
+    gifsicle: null,
 };
 
 /**
+ * 
+ * @returns {Object}
+ */
+async function buildAssetPluginConfig() {
+    const metroConfig = await Metro.loadConfig();
+    const transformerOptions = metroConfig.transformer || {};
+    return Object.assign({}, defaultConfig, transformerOptions.imageminAssetPlugin);
+}
+
+/**
+ * 
+ * @param {Object} config 
+ * @returns {Function[]}
+ */
+function buildImageminPlugins(config) {
+    let plugins = [];
+    for (const [plugin, pluginOptions] of [
+        [imageminMozjpeg, config.mozjpeg],
+        [imageminPngquant, config.pngquant],
+        [imageminGifLossy, config.giflossy],
+        [imageminSvgO, config.svgo],
+        [imageminWebP, config.webp],
+        [imageminJpegTran, config.jpegtran],
+        [imageminOptiPng, config.optipng],
+        [imageminGifSicle, config.gifsicle],
+    ]) {
+        if (pluginOptions != null) {
+            plugins.push(plugin(pluginOptions));
+        }
+    }
+    return plugins;
+}
+
+/**
  * imagemin assetPlugin
- * @param {*} assetData
- * @returns
+ * @param {Object} assetData
+ * @returns {Object}
  */
 async function _imageminAssetPlugin(assetData) {
     try {
-        const metroConfig = await Metro.loadConfig();
-        const transformerOptions = metroConfig.transformer || {};
-        const _options = Object.assign({}, defaultConfig, transformerOptions.imageminAssetPlugin);
-    
+        const options = await buildAssetPluginConfig();
+
+        // TODO:excludes ?
         if (!/node_modules/.test(assetData.fileSystemLocation)) {
             const outputDirectory = path.join(
                 process.cwd(),
-                _options.imageminDir,
+                options.imageminDir,
                 assetData.httpServerLocation
             );
-            if (/\.(png|jpg|jpeg)$/.test(assetData.files[0])) {
+            if (options.test.test(assetData.files[0])) {
                 const tmpFiles = await imagemin(assetData.files, {
                     destination: outputDirectory,
-                    plugins: [imageminPngquant(_options.pngquant), imageminMozjpeg(_options.mozjpeg)],
+                    plugins: buildImageminPlugins(options),
                 });
                 const outFiles = tmpFiles.map(file => file.destinationPath);
                 return {

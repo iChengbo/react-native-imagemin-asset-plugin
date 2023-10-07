4ada9dfbe96c60f32b37d59ac4766300e448509d
iChengbo <chengbo_x@163.com>
2023-10-07 09:48:48 +0800

    chore: add expo-based playground

---
+++ playground/metro.config.js
@@ -0,0 +1,10 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// use plugin to compress assets
+config.transformer.assetPlugins.push('react-native-imagemin-asset-plugin')
+
+module.exports = config;
